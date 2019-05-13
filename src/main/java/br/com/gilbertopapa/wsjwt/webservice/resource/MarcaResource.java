2c2ba46501fce2977d06ef61c7e5c0d6a9707a7d
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-13 14:51:37 -0300

    recursos e permissões

--- src/main/java/br/com/gilbertopapa/wsjwt/webservice/resource/MarcaResource.java
+++ src/main/java/br/com/gilbertopapa/wsjwt/webservice/resource/MarcaResource.java
@@ -1,4 +1,75 @@
 package br.com.gilbertopapa.wsjwt.webservice.resource;
 
+
+import br.com.gilbertopapa.wsjwt.domian.Marca;
+import br.com.gilbertopapa.wsjwt.service.MarcaService;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@Path("/marcas")
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@PermitAll
 public class MarcaResource {
+
+    private final MarcaService marcaService = new MarcaService();
+
+    @POST
+    public Response salvarMarca(Marca marca, @Context UriInfo uriInfo) {
+
+        marcaService.salvarMarca(marca);
+
+        return Response.created(uriInfo.getAbsolutePathBuilder()
+                .path(Long.toString
+                        (marca.getId())).build())
+                .entity(marca)
+                .build();
+    }
+
+    @GET
+    public List<Marca>recuperarMarcas(@QueryParam("nome")
+                                             @DefaultValue("") String nome) {
+        return (nome.isEmpty()) ? marcaService.recuperarMarcas() :
+                marcaService.recuperarMarcasPorNome(nome);
+    }
+
+    @GET
+    @Path("")
+    public Marca recuperarMarcaPorId(@PathParam("marcaId")
+                                             long marcaId) {
+        return marcaService.recuperarMarcaPorId(marcaId);
+    }
+
+
+    @PUT
+    @Path("")
+    @RolesAllowed({"ADMIN", "SUPERVISOR"})
+    public Marca atualizarMarca(@PathParam("marcaId")
+                                        long marcaId, Marca marca) {
+        marcaService.atualizarMarca(marca, marcaId);
+        return marca;
+    }
+
+
+    @DELETE
+    @Path("")
+    @RolesAllowed({"ADMIN"})
+    public void excluirMarca(@PathParam("marcaId") long marcaId) {
+        marcaService.excluirMarca(marcaId);
+    }
+
+
+
+    @Path("/produtos")
+    public ProdutoResource obterProdutoResource() {
+        return new ProdutoResource();
+    }
+
 }
--- src/main/java/br/com/gilbertopapa/wsjwt/webservice/resource/ProdutoResource.java
+++ src/main/java/br/com/gilbertopapa/wsjwt/webservice/resource/ProdutoResource.java
@@ -1,4 +1,69 @@
 package br.com.gilbertopapa.wsjwt.webservice.resource;
 
+
+import br.com.gilbertopapa.wsjwt.domian.Produto;
+import br.com.gilbertopapa.wsjwt.service.ProdutoService;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+@PermitAll
 public class ProdutoResource {
+
+    private final ProdutoService produtoService = new ProdutoService();
+
+    @POST
+    public Response salvarProduto(@PathParam("marcaId")
+                                          long marcaId, Produto produto, @Context UriInfo uriInfo) {
+        produtoService.salvarProduto(produto, marcaId);
+
+        return Response
+                .created(uriInfo.getAbsolutePathBuilder().path
+                        (Long.toString(produto.getId())).build())
+                .entity(produto)
+                .build();
+    }
+
+    @GET
+    public List<Produto> recuperarProdutos(@PathParam("marcaId")
+                                                   long marcaId, @QueryParam("nome") @DefaultValue("") String nome) {
+        return (nome.isEmpty()) ?
+                produtoService.recuperarProdutos(marcaId) :
+                produtoService.recuperarProdutosPorNome(marcaId, nome);
+    }
+
+    @GET
+    @Path("")
+    public Produto recuperarProdutoPorId(@PathParam("marcaId")
+                                                 long marcaId, @PathParam("produtoId") long produtoId) {
+        return produtoService.recuperarProdutoPorId(marcaId, produtoId);
+    }
+
+    @PUT
+    @Path("")
+    @RolesAllowed({"ADMIN", "SUPERVISOR"})
+    public Produto atualizarProduto(@PathParam("marcaId")
+                                            long marcaId, @PathParam("produtoId")
+                                            long produtoId, Produto produto) {
+        produtoService.atualizarProduto(marcaId, produtoId, produto);
+        return produto;
+    }
+
+    @DELETE
+    @Path("")
+    @RolesAllowed({"ADMIN"})
+    public void excluirProduto(@PathParam("produtoId") long produtoId) {
+        System.out.println("Produtoid: " + produtoId);
+        produtoService.excluirProduto(produtoId);
+    }
+
+
 }
