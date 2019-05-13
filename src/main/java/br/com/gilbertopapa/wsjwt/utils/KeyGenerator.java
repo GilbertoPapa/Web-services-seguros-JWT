6e2763b1a299be47e9583b29994446ac57c9b71c
Gilberto Papa <gilberto.papa@digiage.com.br>
2019-05-13 11:45:37 -0300

    gerando token

---
+++ src/main/java/br/com/gilbertopapa/wsjwt/utils/KeyGenerator.java
@@ -0,0 +1,19 @@
+package br.com.gilbertopapa.wsjwt.utils;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
+public class KeyGenerator {
+
+    public Key generateKey(){
+
+
+        String keyString = "R2lsYmVydG8=";
+        //”Gilberto” após SHA-256 e EncodeBase64;;
+
+        Key key = new SecretKeySpec(keyString.getBytes(),
+                0, keyString.getBytes().length, "HmacSHA256");
+
+        return key;
+    }
+}
